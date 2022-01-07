6f56e1bc23ecb2581c5db42854ad1b788b6207e2
gwienc <grzegorz.wienc5@gmail.com>
2022-01-07 13:12:53 +0100

    1. Dodanie klasy encji Medicine 2. Dodanie interfejsu IMedicineRepository do manipulacji danymi encji Medicine

---
+++ Domain/Interfaces/IMedicineRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IMedicineRepository
+    {
+        IQueryable<Medicine> GetAll();
+        Medicine GetById(int id);
+        Medicine Add(Medicine medicine);
+        void Update(Medicine medicine);
+        void Delete(Medicine medicine);
+    }
+}
