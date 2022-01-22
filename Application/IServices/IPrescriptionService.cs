577c1424c26dc20062bb1b085271a2803332460f
gwienc <grzegorz.wienc5@gmail.com>
2022-01-22 11:19:14 +0100

    Utworzenie IPrescriptionService

---
+++ Application/IServices/IPrescriptionService.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTO.Prescription;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IServices
+{
+    interface IPrescriptionService
+    {
+        IEnumerable<PrescriptionDto> GetAllPrescriptions();
+        PrescriptionDto GetPrescriptionById(int id);
+        PrescriptionDto CreatePrescription(CreatePrescriptionDto newPrescription);
+        void UpdatePrescription(int id, UpdatePrescriptionDto prescription);
+        void DeletePrescription(int id);
+    }
+}
